f4011a283959952373b53d44813ede204ea54001
RIABYI <gooopil@i.ua>
2021-01-31 22:04:17 +0200

    lesson3.10_homework

---
+++ Homework/lesson3.10_homework.py
@@ -0,0 +1,121 @@
+# lesson3.10
+
+# EDIT_1
+# Написать маленькую программу, которая на Ваше усмотрение, 
+# реализует полиморфизм. Пример придумать самим
+
+class Toyota:
+    def get_information(self) -> str:
+        """Showing information about the manufacturer"""
+        print('Описание производителя из Википедии')
+    
+class Camry(Toyota):
+    def get_information(self) -> str:
+        """Showing information about the car"""
+        super().get_information()
+        print('Описание модели из Википедии')
+
+class Auris(Toyota):
+    def get_information(self) -> str:
+        """Showing information about the car"""
+        print('Описание модели из Википедии')
+
+mycar = Camry()
+mycar.get_information() 
+
+mycar = Auris()
+mycar.get_information()
+
+# EDIT_2
+# Реализовать клас Температуры (как сегодня в конце урока), который 
+# на основе @property будет возвращать, присваивать данные для цельсиев, 
+# фаренгейтов и кельвинов.
+
+class Temperature:
+    def __init__(self, mark = int, scale = str):
+        self.__mark = mark
+        self.__scale = scale
+           
+    @property
+    def celcius(self):
+        if self.__scale == 'C': 
+            return self.__mark
+        if self.__scale == 'F':
+            return int((self.__mark - 32) * (5/9))
+        if self.__scale == 'K':
+            return int(self.__mark - 273.15)
+        
+    @celcius.setter
+    def celsius(self, value):
+        self.__mark = value
+        self.__scale = 'C'
+
+    @property
+    def fahrenheit(self):
+        if self.__scale == 'C': 
+            return self.__mark + 32
+        if self.__scale == 'F':
+            return self.__mark
+        if self.__scale == 'K':
+            return int((self.__mark - 273.15) * (9/5) + 32)
+        
+    @fahrenheit.setter
+    def fahrenheit(self, value):
+        self.__mark = value
+        self.__scale = 'F'
+
+    @property
+    def kelvin(self):
+        if self.__scale == 'C': 
+            return int(self.__mark + 273.15)
+        if self.__scale == 'F':
+            return int((self.__mark - 32) * (5/9) + 273.15)
+        if self.__scale == 'K':
+            return self.__mark
+
+    @kelvin.setter
+    def kelvin(self, value):
+        self.__mark = value
+        self.__scale = 'K'
+
+a = Temperature (1, 'C')
+print(a.celsius)
+print(a.kelvin)
+
+a.celsius = 22
+print(a.celsius)
+print(a.kelvin)
+
+# EDIT_3s
+# Бонус+ режим: кто сможет закончить пример с ФУНКТОРОМ сортировки ч-з
+# класс получит от меня мастеркласс по чистой архитектуре кода, когда
+# будем изучать ВЕБ )
+
+class SortKey:
+    def __init__(self, *args):
+        self.attrs = args
+    def __call__(self,	instance):
+        values = []
+        for attr in self.attrs:
+            values.append(getattr(instance, attr)) 
+        return values
+
+class Person:
+    def __init__(self,	name, surname, email):
+        self.name = name 
+        self.surname = surname 
+        self.email = email
+
+    def __repr__(self):
+        return self.name
+
+joe = Person('Joe', 'QWE', 'joe@mail.com')
+mark = Person('Mark', 'QWasdE', 'mark@mail.com')
+jack = Person('Jack', 'Q123WqweqeE', 'jack@mail.com')
+people = [joe, mark, jack]
+
+print(people) # OUTPUT: [Joe, Mark, Jack]
+
+people.sort(key = SortKey('name'))
+
+print(people) # OUTPUT: [Jack, Joe, Mark] 
